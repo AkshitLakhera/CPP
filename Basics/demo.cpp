81682b5c943e4a60d0b5413e424b88a86b9f6ae0
“[Akshit <akshitlakhera14@gmail.com>
2024-05-16 22:56:21 +0530

    basics added

---
+++ Basics/demo.cpp
@@ -0,0 +1,57 @@
+// #include <iostream>
+// int main()
+// {
+//     std::cout << "Hey Akshit " << "\n"
+//               << "Hey Piyush" << std::endl
+//               << "Hey gunnu";
+//     return 0;
+// }
+// Here first we import a library called iostream to  get input and output
+// cout is used to print something
+//  std is a basic library we use it with everything further we will use namespace
+
+//                          Namespace and taking input
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int x, y;
+//     cin >> x >> y; // to take input
+//     cout << "The value of x is  :" << x << " and the value of y is " << y;
+// }
+
+//                          Data type
+
+#include <bits/stdc++.h> // to access all the libraries
+using namespace std;
+int main()
+{
+    // // int
+    // int x = 5 ;
+    // // long
+    // long x = 15; // In long we  have bit wider ranger
+    // //  for more wider range
+    // long long x = 15000000;
+
+    // // Float to  get decimal values
+    // float y = 10 ;
+    // float m = 10.6;
+    // // Double to get decimal value in long range integer can also be saved in double
+    // double y= 12;
+
+    // For storing string values
+    // string s1;
+    // string s2;
+    // cin >> s1 >> s2;
+    // cout << s1 << " " << s2;
+    // return 0;
+
+    //  Storing whole string line we use get line .String take much space
+    // string s1;
+    // getline(cin, s1);
+    // cout << s1;
+    //  char
+    char ch = 'y';
+    cout << ch;
+    return 0;
+}
